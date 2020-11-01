c37e9c748e5c22ea0c3cbc2192c083154242ddea
Josimar Bruno Benvenutti <68384@PROWAY.TREINA>
2020-11-01 15:52:25 -0300

    interface para conexao com bando de dados

---
+++ src/main/java/br/com/agenda/data/Conexao.java
@@ -0,0 +1,16 @@
+package br.com.agenda.data;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface Conexao {
+	
+	Connection getConnection();
+	
+	void close();
+	
+	void commit() throws SQLException;
+	
+	void rollbacku();
+	
+}
